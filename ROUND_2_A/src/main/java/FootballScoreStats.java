8de9ccb5c8169892eeb6d3ebd64a7b88c2b4d58c
Kristian Jakobsen <kristian@nymann.dev>
2024-08-10 21:51:39 +0200

    Inline all and remove method - 2

--- ROUND_2_A/src/main/java/FootballScoreStats.java
+++ ROUND_2_A/src/main/java/FootballScoreStats.java
@@ -16,7 +16,11 @@ public class FootballScoreStats {
 	}
 
 	private Game[] getAllPlayed() {
-		return FootballData.getAllPlayed();
+		Game[] played = new Game[3];
+		played[0] = new Game("Liverpool", 2, "Everton", 0);
+		played[1] = new Game("Aston Villa", 1, "Liverpool", 1);
+		played[2] = new Game("Liverpool", 3, "West Ham", 1);
+		return played;
 	}
 
 }
